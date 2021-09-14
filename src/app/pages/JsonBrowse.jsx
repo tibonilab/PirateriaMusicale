9cbb3e48c32e873784452092e5593e65fc1dfea8
Alberto Tiboni <a.tiboni@gmail.com>
2021-09-14 17:12:12 +0200

    showing browse fetch error

--- src/app/pages/JsonBrowse.jsx
+++ src/app/pages/JsonBrowse.jsx
@@ -14,39 +14,28 @@ import Collapsible from '../components/template/components/Collapsible.jsx';
 import { PrimaryButton } from '../components/template/components/Buttons.jsx';
 
 import { t } from '../i18n';
-// import { browse } from '../model/Solr';
-
-// const indexes = () => [
-//     { value: 'inventory', label: t('common.indexes.inventory') },
-//     { value: 'composer_names', label: t('common.indexes.composer_names') },
-//     { value: 'other_names', label: t('common.indexes.other_names') },
-//     { value: 'original_call_no', label: t('common.indexes.original_call_no') },
-//     { value: 'call_no', label: t('common.indexes.call_no') },
-// ];
 
 const indexes = () => [
-    { value: 'Names', label: 'Names' },
-    // { value: 'Dates', label: 'Dates' },
-    // { value: 'Feasts', label: 'Feasts' },
+    { value: 'Composers', label: 'Compositori' },
+    { value: 'Toc', label: 'Sommario' },
 ];
 
 const JsonBrowse = () => {
 
-    const { performBrowse, browseResults, loadingBrowse, loadRelated, loadingRelated, related } = useContext(CurstomContext);
+    const {
+        performBrowse,
+        browseResults,
+        loadingBrowse,
+        loadRelated,
+        loadingRelated,
+        related,
+        browseError
+    } = useContext(CurstomContext);
 
     const [selectedIndex, setSelectedIndex] = useStateWithSession('', 'selectedIndex', 'CustomState');
-    const [results, setResults] = useStateWithSession([], 'results', 'CustomState');
-    const [isButtonDisabled, setIsButtonDisabled] = useState(!/\S/.test(selectedIndex), 'isButtonDisabled', 'CustomState');
-    const [buttonLabel, setButtonLabel] = useState(t('browse.form.submit'));
-    // const [related, setRelated] = useState({});
-
-    // const appendRelated = (group) => {
-    //     setRelated({ ...related, ...group });
-    // };
 
     const selectChangeHandler = value => {
         const testValue = /\S/.test(value);
-        setIsButtonDisabled(!testValue);
         testValue && setSelectedIndex(value);
     };
 
@@ -71,33 +60,35 @@ const JsonBrowse = () => {
                         <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
                         <h4>Loading data, please wait..</h4>
                     </FlexWrapper>
-                    : browseResults && browseResults/* .slice(0, 3) */.map((e, key) => (
-                        <React.Fragment key={e.name}>
-                            <Collapsible
-                                key={key}
-                                header={(<h3 className="collapsible-header-caption" style={{ borderBottom: '1px solid #e8e8e8', display: 'block', width: '100%', paddingBottom: '.5em' }}>{e.name}</h3>)}
-                                onClickHandler={collapsed => !collapsed && loadRelated({ index: selectedIndex, params: { key, name: e.name } })}>
+                    : browseError
+                        ? <div>{'Unable to load index data'}</div>
+                        : browseResults && browseResults/* .slice(0, 3) */.map((e, key) => (
+                            <React.Fragment key={e.name}>
+                                <Collapsible
+                                    key={key}
+                                    header={(<h3 className="collapsible-header-caption" style={{ borderBottom: '1px solid #e8e8e8', display: 'block', width: '100%', paddingBottom: '.5em' }}>{e.name}</h3>)}
+                                    onClickHandler={collapsed => !collapsed && loadRelated({ index: selectedIndex, params: { key, name: e.name } })}>
 
-                                {
-                                    related[`${key}_${e.name}`] && isLoadingRelated(key, e.name) && <div><div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-                                        <h4>Loading data, please wait..</h4></div>
-                                }
+                                    {
+                                        isLoadingRelated(key, e.name) && <div><div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                                            <h4>Loading data, please wait..</h4></div>
+                                    }
 
-                                {
-                                    related[`${key}_${e.name}`] && (!isLoadingRelated(key, e.name) && related[`${key}_${e.name}`] && Array.isArray(related[`${key}_${e.name}`])) && <div><ul>{
-                                        related[`${key}_${e.name}`].map((data, index) => {
-                                            console.log(data);
+                                    {
+                                        related[`${key}_${e.name}`] && (!isLoadingRelated(key, e.name) && related[`${key}_${e.name}`] && Array.isArray(related[`${key}_${e.name}`])) && <div><ul>{
+                                            related[`${key}_${e.name}`].map((data, index) => {
+                                                console.log(data);
 
-                                            return (
-                                                <li key={index}>{data.label} <Link to={`/book#${data.target}`}>Go</Link></li>
-                                            )
+                                                return (
+                                                    <li key={index}>{data.label} <Link to={`/book#${data.target}`}>Go</Link></li>
+                                                );
 
-                                        })}</ul></div>
-                                }
-                            </Collapsible>
+                                            })}</ul></div>
+                                    }
+                                </Collapsible>
 
-                        </React.Fragment>
-                    ))
+                            </React.Fragment>
+                        ))
             }
         </Template >
     );
