30dfefa96186407744232bdbdce3fb8ffdcb58d1
Alberto Tiboni <a.tiboni@gmail.com>
2021-09-21 16:40:57 +0200

    cleanup

--- src/app/components/shared/TocContextBar.jsx
+++ src/app/components/shared/TocContextBar.jsx
@@ -14,7 +14,6 @@ export const TocContextBar = ({ visible, toggleBar }) => {
 
     const toc = index.index.group[0].group;
 
-    console.log(toc);
 
     return (
         <ContextBar visible={visible} toggleBar={toggleBar}>
@@ -22,7 +21,6 @@ export const TocContextBar = ({ visible, toggleBar }) => {
                 <h4>Table of Contents</h4>
                 {toc.map(e => <Collapsible key={e.name} header={<div><h5>{e.name}</h5><span><small>{e.subtitle}</small></span></div>}>
                     {e.link && <ul>{e.link.map((link, k) => {
-                        console.log(link.label);
                         return <li key={k}><a href={`/book#${link.target}`}>{link.label.replace(' ', '\u00a0')}</a></li>;
                     })}</ul>}
                 </Collapsible>)}
